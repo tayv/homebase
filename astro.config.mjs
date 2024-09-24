e9e90d1417fff3a9d3a647fa2b6432499daf632a
tayv <48400779+tayv@users.noreply.github.com>
2024-09-23 21:29:31 -0600

    feat: update config for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from "astro/config"
 
 // https://astro.build/config
+
 export default defineConfig({
-  site: "https://tayv.github.io",
-  base: "/homebase/",
+  site: "https://taylorv.com", // "https://tayv.github.io",
+  // base: "/homebase",  // https://docs.astro.build/en/guides/deploy/github/#base leave out since using a custom domain
 })
