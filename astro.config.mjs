739ca22a3d3ca1510583c98c28496a463d60f8a3
tayv <48400779+tayv@users.noreply.github.com>
2024-09-29 17:37:45 -0600

    fix: add tailwind integration
    to fix failed styles.css import after build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config"
-
+import tailwind from "@astrojs/tailwind"
 // https://astro.build/config
 
 export default defineConfig({
   site: "https://taylorv.com", // "https://tayv.github.io",
   // base: "/homebase",  // https://docs.astro.build/en/guides/deploy/github/#base leave out since using a custom domain
+  integrations: [tailwind()],
 })
